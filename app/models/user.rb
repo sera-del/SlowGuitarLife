f2577b5193af2824bf80b384e65daf346dece712
osera <keeeeen2020@gmail.com>
2023-11-05 15:58:58 +0900

    userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :profile, length: { maximum: 20 }
+  validates :guitar_type, length { maximum: 10 }
+  validates :years_experience, length { maximum: 2 }
 end
