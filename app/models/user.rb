48260722ec4c242861b0db6431d392b4fc3fa0eb
osera <keeeeen2020@gmail.com>
2023-11-05 23:08:48 +0900

    userモデルのバリデーションを変更
    8行目のバリデーションの最大文字数を50に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :profile, length: { maximum: 20 }
+  validates :profile, length: { maximum: 50 }
   validates :guitar_type, length: { maximum: 10 }
   validates :years_experience, length: { maximum: 2 }
 end
