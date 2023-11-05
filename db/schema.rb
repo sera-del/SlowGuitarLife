9e456371a2708155965b3ba9f979923a3826c766
osera <keeeeen2020@gmail.com>
2023-11-05 15:47:21 +0900

    usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_31_061618) do
+ActiveRecord::Schema.define(version: 2023_11_05_064359) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2023_10_31_061618) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "user_img"
+    t.string "profile"
+    t.string "guitar_type"
+    t.integer "years_experience"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
