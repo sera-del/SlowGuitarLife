76e67ba3520dd001b84f3bb2194671672f14942d
osera <keeeeen2020@gmail.com>
2023-11-07 01:43:38 +0900

    userテーブルにcategoryカラムを追加
    アカウント情報に音楽ジャンルを追加するためにcategoryカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_06_140905) do
+ActiveRecord::Schema.define(version: 2023_11_06_150154) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2023_11_06_140905) do
     t.string "profile"
     t.string "guitar_type"
     t.integer "years_experience"
+    t.string "category"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
