6df064bb37c961a6b5f3393372369860b1b9566c
osera <keeeeen2020@gmail.com>
2023-11-07 01:37:46 +0900

    userモデルのバリデーションを変更
    years_experienceのバリデーションを100より大きい数値にできないように制限をかけた

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :profile, length: { maximum: 50 }
   validates :guitar_type, length: { maximum: 10 }
-  validates :years_experience, length: { maximum: 2 }
+  validates :years_experience, numericality: { less_than: 100 }
 
   has_one_attached :user_img
 end
