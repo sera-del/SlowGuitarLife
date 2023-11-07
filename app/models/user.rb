0f3cd3473fb319350269a06e49d965dfd0c3140f
osera <keeeeen2020@gmail.com>
2023-11-08 01:04:53 +0900

    意図しないバリデーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :profile, length: { maximum: 50 }
   validates :guitar_type, length: { maximum: 10 }
-  validates :years_experience, numericality: { less_than: 100 }
 
   has_one_attached :user_img
 end
