61006869c3a6bbd73276ae0c68659b7e20f27ba5
osera <keeeeen2020@gmail.com>
2023-11-12 18:08:35 +0900

    ルーティング設定の変更
    devise_forから使ってないsessionsコントローラーを削除。userのresourcesを
    showのみに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions:      "users/sessions"
+    registrations: 'users/registrations'
   }
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get 'users/sign_out', to: 'devise/sessions#destroy'
   end
 
   root to: 'homes#index'
+
   get 'users/account', to: 'users#account'
-  resources :users
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
