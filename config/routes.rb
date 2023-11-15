c684377ab708744a85024fcb4fcb92a8807368e4
osera <keeeeen2020@gmail.com>
2023-11-15 17:53:46 +0900

    ゲストログイン機能のためにルーティング追加
    devise_scopeにルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
+
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do
-    get 'users/sign_out', to: 'devise/sessions#destroy'
+    get 'users/sign_out',       to: 'devise/sessions#destroy'
+    get 'users/guest_sign_in',  to: 'users/sessions#guest_sign_in'
   end
 
-  root to: 'homes#index'
+  get 'users/account',    to: 'users#account'
+  get 'users/check',      to: 'users#check'
+  get 'users/withdraw',   to: 'users#withdraw'
 
-  get 'users/account', to: 'users#account'
   resources :users, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
