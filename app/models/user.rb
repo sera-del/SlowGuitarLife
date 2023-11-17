feb549effb12b4eb6325bd62e8feaa2b73f0d815
osera <keeeeen2020@gmail.com>
2023-11-17 23:44:24 +0900

    ゲストユーザー作成もしくは探すメソッドを追加
    ゲストユーザーを探すメソッド。見つからなければ作成する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.username = "ゲスト"
+    end
+  end
+
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
+
   validates :username, presence: true
   validates :profile, length: { maximum: 50 }
   validates :guitar_type, length: { maximum: 10 }
