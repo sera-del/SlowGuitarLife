3b6bd33edc8d35907027d9326c068954adf82e2c
osera <keeeeen2020@gmail.com>
2023-11-17 23:37:40 +0900

    usersコントローラーに退会機能を実装
    withdrawアクションに退会機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def account
     @user = current_user
   end
+
+  def withdraw
+    @user = User.find(current_user.id)
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
 end
