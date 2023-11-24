0fb6282f1979e101a51c9c9dac2b90d2d58c3d86
osera <keeeeen2020@gmail.com>
2023-11-24 23:59:38 +0900

    ページにアクセスできているテストを追加

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/homes/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /" do
+
+    before do
+      get root_path
+    end
+
+    it "HTTPレスポンスコードが200を返すこと" do
+      expect(response).to have_http_status(200)
     end
   end
-
 end
