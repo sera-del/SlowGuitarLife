71cbf3a90bb6edb4b530b161019a42ef8f026bad
osera <keeeeen2020@gmail.com>
2023-11-27 13:30:47 +0900

    Rspecで使用するテストデータを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    username { "test_user" }
+    sequence(:email) { |n| "test_#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    guitar_type { "エレキギター" }
+    years_experience { "3年" }
+    category { "ロック" }
   end
 end
