771de2fbd085311ad61782ffef9974074d4e70b4
osera <keeeeen2020@gmail.com>
2023-11-29 01:57:25 +0900

    モデルスペックを実装
    ゲストユーザーの作成と退会したユーザーがログインできない制約を確認するメソッドを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#guest" do
+    context "ゲストユーザーが作成された場合" do
+      it "ユーザー名がゲストになっていること" do
+        user = User.guest
+        expect(user.username).to eq "ゲスト"
+      end
+    end
+  end
+
+  describe "#active_for_authentication?" do
+    context "退会している場合" do
+      it "trueを返すこと" do
+        user = User.new(
+          username: "user",
+          email: "user@example.com",
+          is_deleted: false
+        )
+        expect(user.active_for_authentication?).to eq true
+      end
+    end
+  end
 end
