fb60e2985d8c08f09d50131e26a282996dbd622a
osera <keeeeen2020@gmail.com>
2023-12-06 03:11:05 +0900

    新規投稿フォームにバリデーションを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :content, presence: true, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["クラシック", "ロック", "ポップス", "ジャズ", "その他"] }
 end
