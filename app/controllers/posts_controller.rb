9d76a40b9823bd6590e66916fa6f9d264f032eea
osera <keeeeen2020@gmail.com>
2023-12-06 03:06:15 +0900

    投稿機能を実装
    ユーザーを紐付けた状態での投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    @users = User.all
+    @posts = Post.all.includes(:user)
   end
 
   def new
+    @post = Post.new
+    @user = current_user
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to users_account_path(current_user), notice: "新規投稿をしました"
+    else
+      render :new
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -20,5 +28,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category, :tag).merge(user_id: current_user.id)
   end
 end
