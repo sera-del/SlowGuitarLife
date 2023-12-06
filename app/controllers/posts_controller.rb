e281fe2be8a942048c298aacd76a21eb553ac5b1
osera <keeeeen2020@gmail.com>
2023-12-06 19:10:34 +0900

    更新と削除の機能を追加
    投稿記事の更新と削除を追加した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,12 +22,22 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to user_path(current_user), notice: "投稿を更新しました"
+    else
+      redirect_to edit_post_path, alert: "更新に失敗しました"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user), notice: "投稿を削除しました"
   end
 
   private
