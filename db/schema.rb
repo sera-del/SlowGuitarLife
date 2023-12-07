c89255705915959f6df89fb2cb3b8d554ff315be
osera <keeeeen2020@gmail.com>
2023-12-07 22:58:16 +0900

    カラム名を変更
    カラム名が相応しくなかったので、見てわかるカラム名に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_07_133903) do
+ActiveRecord::Schema.define(version: 2023_12_07_135618) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2023_12_07_133903) do
     t.integer "user_id"
     t.string "title"
     t.text "content"
-    t.string "video"
+    t.string "youtube_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
