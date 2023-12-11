c483f26d9cbe6973dbaf28a66bf7ece0e5d2b8e4
osera <keeeeen2020@gmail.com>
2023-12-11 17:22:02 +0900

    factorybotにデータを作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :post do
     user_id { 1 }
-    title { "MyString" }
-    content { "MyText" }
-    tag { "MyString" }
+    title { "音階練習" }
+    content { "ドレミファソラシドを低いドから高いドまで順番に弾いてみましょう。" }
+    category { "クラシック" }
+    youtube_url { "NSuiLcamM5s" }
   end
 end
