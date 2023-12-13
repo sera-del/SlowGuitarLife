a237ceac99a1090e562d1f53d9627226672ed52f
osera <keeeeen2020@gmail.com>
2023-12-14 00:53:49 +0900

    tagカラムにバリデーションを追加
    新規投稿、投稿編集でtagを必ず入力させるため

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 250 }
   validates :category, inclusion: { in: ["クラシック", "ロック", "ポップス", "ジャズ", "その他"] }
+  validates :tag, presence: true
 end
