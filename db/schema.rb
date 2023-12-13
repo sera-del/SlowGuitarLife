d2b8a9c7dc6c20f636aa58272f86653443715b70
osera <keeeeen2020@gmail.com>
2023-12-13 23:36:32 +0900

    Postモデルにtagカラムを追加
    タグ検索をするためにtagカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_07_135618) do
+ActiveRecord::Schema.define(version: 2023_12_13_143459) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2023_12_07_135618) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
+    t.string "tag"
   end
 
   create_table "users", force: :cascade do |t|
