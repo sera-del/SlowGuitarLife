0fc2cefce0ebad217e03358cfa4252df44c42e69
osera <keeeeen2020@gmail.com>
2023-12-15 17:21:16 +0900

    postコントローラーで使う検索のメソッドを追加
    あいまい検索のコードをselfを用いて追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 250 }
   validates :category, inclusion: { in: ["クラシック", "ロック", "ポップス", "ジャズ", "その他"] }
   validates :tag, presence: true
+
+  def self.search(search)
+    return Post.all unless search
+    Post.where("title LIKE(?)", "%#{search}%").or(where("category LIKE(?)", "%#{search}%"))
+  end
 end
