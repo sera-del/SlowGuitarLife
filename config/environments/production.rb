e5e7fe2ed3bea90e4542baecdddf83ef00e37332
osera <keeeeen2020@gmail.com>
2023-12-25 23:35:44 +0900

    本番環境でパスワード再設定できるように追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'https://slow-guitar-life-1e95e25b613d.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.mailer[:mail_address],
+    password:             Rails.application.credentials.mailer[:password],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
 end
